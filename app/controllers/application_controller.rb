cd62d90da905b5ffcd6f78418fa3e3faefa6cccf
Raj Singh <rsingh40@chicagobooth.edu>
2023-04-12 00:07:30 +0000

    first draft

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-
+  def play_rock
+    # redirect_to("https://www.wikipedia.org")
+    # render({:plain => "Please work. Please."})
+    render({:template => "game_templates/user_rock.html.erb"})
+  end
 end
