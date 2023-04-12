ae2778d4b0837ee0a3690cb0ce9837a098dcb7d3
Raj Singh <rsingh40@chicagobooth.edu>
2023-04-12 00:43:47 +0000

    third draft

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,45 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
   def homepage
-    
+    render ({:template => "game_templates/rules.html.erb"})
   end
  
   def play_rock
-    # redirect_to("https://www.wikipedia.org")
-    # render({:plain => "Please work. Please."})
     @comp_move = ["rock", "paper", "scissors"].sample
-    if comp_move == "rock"
+    if @comp_move == "rock"
       @outcome = "tied"
-    elsif comp_move == "paper"
+    elsif @comp_move == "paper"
       @outcome = "lost"
     else
       @outcome = "won"
     end
-
     render({:template => "game_templates/user_rock.html.erb"})
   end
 
   def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
     render({:template => "game_templates/user_paper.html.erb"})
   end
 
   def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
     render({:template => "game_templates/user_scissors.html.erb"})
   end
 
