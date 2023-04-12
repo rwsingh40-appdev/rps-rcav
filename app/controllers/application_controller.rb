bc56d37b02dd952603a955e881b4c5beddc5f6bb
Raj Singh <rsingh40@chicagobooth.edu>
2023-04-12 00:17:18 +0000

    second draft

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,31 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def homepage
+    
+  end
+ 
   def play_rock
     # redirect_to("https://www.wikipedia.org")
     # render({:plain => "Please work. Please."})
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if comp_move == "rock"
+      @outcome = "tied"
+    elsif comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
     render({:template => "game_templates/user_rock.html.erb"})
   end
+
+  def play_paper
+    render({:template => "game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors
+    render({:template => "game_templates/user_scissors.html.erb"})
+  end
+
 end
